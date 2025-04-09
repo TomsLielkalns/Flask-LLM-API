9cde33e752fa434da9d97faf4166e0c195d4c72d
Toms Lielkalns <lielkalnstoms@gmail.com>
2025-04-09 19:19:15 +0300

    better text handling without newlines and max content length

--- backend/app.py
+++ backend/app.py
@@ -4,15 +4,18 @@ from helpers.send_req_to_api import send_to_hf
 from helpers.parse_file import parse_file
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024  # 2MB
 
 
 @app.route("/summerize", methods=["POST"])
 def summerize():
-    text = request.get_data(as_text=True)
     file = request.files.get("file")
-
+    text = ""
+    
     if file:
         text = parse_file(file, abort)
+    else:
+        text = request.get_data(as_text=True).strip()
 
     if not text:
         return jsonify({"error": "No input text or file provided"}), 400
